1c4d73cf97608000280e0b75cd31be3a6c27b3f8
Jaka Hudoklin <jakahudoklin@gmail.com>
2017-07-29 23:08:34 +0200

    Add script for funding

---
+++ scripts/fund.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+COLD_WALLET=$(cat cold_wallet.json | jq -r '.address')
+COLD_SECRET=$(cat cold_wallet.json | jq -r '.secret')
+HOT_WALLET=$(cat hot_wallet.json | jq -r '.address')
+
+ripple-tools send-payment \
+	--address $COLD_WALLET --secret $COLD_SECRET \
+	--to $HOT_WALLET --amount 100000 --currency USD
